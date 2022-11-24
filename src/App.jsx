ac2af357534bbcf064334def04c592f25485bf26
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-24 13:37:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,8 +62,7 @@ function App() {
       {
         entryPriceFilename = (Number(entryPrice)+((downloadCount+1)*0.5)).toFixed(1);
       }
-      const name =
-      switch(downloadCount)
+      const name = switch(downloadCount)
         {
            case 0: name = "정지호"; break;
            case 1: name = "최성준"; break;
