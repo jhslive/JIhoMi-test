b0b10de1ae2efc5358ca8fe7a4919cef6c4bf717
jhslive <111948596+jhslive@users.noreply.github.com>
2023-05-08 23:21:28 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -247,7 +247,7 @@ function App() {
 
             }}
           >
-            {console.log(phone.charAT(1,2,3))}***{console.log(phone.charAT(7,8,9))}
+            {console.log(phone.charAT(1,2,3))***console.log(phone.charAT(7,8,9,10))}
           </div>
           <div
             style={{
