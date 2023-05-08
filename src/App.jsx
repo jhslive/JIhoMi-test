ceebffb41c386d1f3c5a720a9e7c8171fc079db0
jhslive <111948596+jhslive@users.noreply.github.com>
2023-05-09 00:15:53 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     ((closingPrice / entryPrice - 1) * 75 * 100).toFixed(2)
   );
   const [tangu, setTangu] = useState(false);
-  const [cofo, setCofo] = (console.log(phone.charAt(0,1,2)));
+  const cofo = (console.log(phone.charAt(0,1,2)));
   
   useEffect(() => {
     const calculated = (
