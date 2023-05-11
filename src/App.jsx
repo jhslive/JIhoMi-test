58d92c816d66f0374c1f981799511fa090c7981b
jhslive <111948596+jhslive@users.noreply.github.com>
2023-05-11 22:30:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -168,7 +168,7 @@ function App() {
             style={{
               position: "absolute",
               left: numLocLR + 85 + "px",
-              top: numsLoc[0] + 160 + "px",
+              top: numsLoc[0] + 260 + "px",
               fontSize:"42px",
               color: "rgb(254,254,254)",
               fontFamily: "HarmonyOS Sans",
